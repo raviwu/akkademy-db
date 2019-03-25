c2753e67b57febb33fc760a758d4238bcb7dc2d0
Ravi Wu <raviwu@gmail.com>
2019-03-25 14:41:51 +0800

    Initialize repo

---
+++ src/main/java/org/lwstudio/akkademy/AkkademyDb.java
@@ -0,0 +1,23 @@
+package org.lwstudio.akkademy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lwstudio.akkademy.messages.SetRequest;
+
+import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class AkkademyDb extends AbstractActor {
+    protected final LoggingAdapter log = Logging.getLogger(context().system(), this);
+    protected final Map<String, Object> map = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().match(SetRequest.class, message -> {
+            log.info("Received set request - key: {} value: {}", message.getKey(), message.getValue());
+            map.put(message.getKey(), message.getValue());
+        }).matchAny(o -> log.info("received unknown message {}", o)).build();
+    }
+}
---
+++ src/test/java/org/lwstudio/akkademy/AkkademyDbTest.java
@@ -0,0 +1,25 @@
+package org.lwstudio.akkademy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.lwstudio.akkademy.messages.SetRequest;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.testkit.TestActorRef;
+
+public class AkkademyDbTest {
+    ActorSystem system = ActorSystem.create();
+
+    @Test
+    public void itShouldPlaceKeyValueFromSetRequestMessageIntoMap() {
+        TestActorRef<AkkademyDb> actorRef = TestActorRef.create(system, Props.create(AkkademyDb.class));
+
+        actorRef.tell(new SetRequest("key", "value"), ActorRef.noSender());
+
+        AkkademyDb akkademyDb = actorRef.underlyingActor();
+        assertEquals("value", akkademyDb.map.get("key"));
+    }
+}
